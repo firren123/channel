fea79fb77fa28ecd9448a7c03d0a9e7e94047b34
renyineng <renyineng@iyangpin.com>
2015-10-19 13:49:27 +0800

    cookies

--- backend/config/main.php
+++ backend/config/main.php
@@ -6,12 +6,4 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'defaultRoute'=>'site/index',
     'timeZone'=>'Asia/Chongqing',
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'ZIEF78AHhhkekr-wri32jgekjkwerkkjfwfwa',
-            'enableCookieValidation' => false,
-            'enableCsrfValidation' => FALSE,
-        ]
-    ]
 ];
